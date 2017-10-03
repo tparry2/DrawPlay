61409e11750c2218ef79b2f81c527967eb576dcd
Jessy Liao <jessyliao@mines.edu>
2017-10-03 14:47:56 -0600

    made eyes black like it

--- src/Cat.java
+++ src/Cat.java
@@ -30,7 +30,7 @@ public class Cat {
 		g2.setColor(Color.yellow);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.red);
+		g2.setColor(Color.black);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
